274e1103f8fff07b91edf5f927c76f0c033ccff9
hrishi0102 <hrishi0102business@gmail.com>
2025-03-20 18:48:50 +0530

    test payee

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ const payee = await payman.payments.createPayee({
 		email: "hrishi@example.com",
 	},
 });
-
 // Crypto
 const cryptoPayee = await payman.payments.createPayee({
     type: "CRYPTO_ADDRESS",
@@ -34,14 +33,28 @@ const cryptoPayee = await payman.payments.createPayee({
     },
     tags: ["crypto", "eth"]
 });
+//Test Payee
+const test_payee = await payman.payments.createPayee({
+	type: "TEST_RAILS",
+	name: "Hrishikesh",
+	accountHolderName: "Hrishikesh",
+	accountHolderType: "individual",
+	accountNumber: "12345678",
+	routingNumber: "021000021",
+	accountType: "checking",
+	contactDetails: {
+		email: "hrishikesh@example.com",
+	},
+});
 
+console.log(`Test Payee created: ${test_payee.id}`);
 console.log(`Payee created: ${payee.id}`);
 console.log(`Crypto Payee created: ${cryptoPayee.id}`);
 
-//Send a payment (can only use Test Account)
+//Send a payment (depends on payee type)
 const payment = await payman.payments.sendPayment({
     amountDecimal: 5.00,
-    payeeId: 'pd-1f0056c2-e3bc-6593-82c2-2f0e5fd60eb7',
+    payeeId: 'pd-1f0058d6-7d35-6cb7-845a-879e3cf2ecf4',
     memo: 'Invoice #1234',
     metadata: {
       department: "marketing"
@@ -50,11 +63,10 @@ const payment = await payman.payments.sendPayment({
 
 console.log('Payment sent:', JSON.stringify(payment));
 
-//Search Payee
+//Search Payee (depends on payee type)
 const all_payees = await payman.payments.searchPayees();
 const payees_filtered = await payman.payments.searchPayees({
-    name: "Hrishi",
-    type: "US_ACH"
+    name: "Hrishikesh"
 });
 
 console.log('All payees:', JSON.stringify(all_payees));
